3877340d1dc2a6d098fe6a03c53c3317da9f311e
trunlel <langodarlan46@gmail.com>
2023-01-15 00:25:14 -0300

    Migrations

---
+++ src/core/database/migrations/1673751427854-$npm_config_name.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class $npmConfigName1673751427854 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
