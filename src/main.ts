ee26b9901fc5904ba8689383b5074926fd42f4ab
trunlel <langodarlan46@gmail.com>
2023-02-18 22:18:48 -0300

    feature: buildspec

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
-
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
